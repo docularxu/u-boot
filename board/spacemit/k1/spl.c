8227a26cb623507a65bd1704f49040b053e801f7
Guodong Xu <guodong@riscstar.com>
2026-05-17 19:44:04 +0800

    spacemit: k1: Add multiple device tree support
    Enable multiple DTB support in the FIT image for the Spacemit K1 SoC,
    allowing a single U-Boot binary to support different board variants.
    
    The SPL reads the board type from EEPROM and selects the corresponding
    device tree at runtime via board_fit_config_name_match(), ensuring the
    correct hardware description is passed to U-Boot proper.
    
    Signed-off-by: Guodong Xu <guodong@riscstar.com>
    
    ---
    v2:
    - Reworked.  Drop the v1 approach (new local k1-muse-pi-pro.dts,
      enlarge SYS_MALLOC_F_LEN, MMODE switch).
    - Use binman --fit-multi-config so u-boot.itb packs multiple board
      DTs from the upstream tree.

--- board/spacemit/k1/spl.c
+++ board/spacemit/k1/spl.c
@@ -13,6 +13,7 @@
 #include <dm/device.h>
 #include <dm/uclass.h>
 #include <i2c.h>
+#include <linux/ctype.h>
 #include <linux/delay.h>
 #include <log.h>
 #include <power/regulator.h>
@@ -54,6 +55,8 @@ struct ddr_cfg {
 
 binman_sym_declare(ulong, ddr_fw, image_pos);
 binman_sym_declare(ulong, ddr_fw, size);
+
+char product_name[I2C_BUF_SIZE] = "k1";
 
 static void i2c_early_init(void)
 {
@@ -102,6 +105,34 @@ int read_product_name(char *name, int size)
 		tlv_entry = (struct tlvinfo_tlv *)p;
 	}
 	return -ENOENT;
+}
+
+static void fixup_product_name(char *name, int size)
+{
+	char fdt_name[I2C_BUF_SIZE];
+	int i;
+
+	memset(fdt_name, 0, I2C_BUF_SIZE);
+	if (!strncmp(product_name, "k1-x_MUSE-Pi-Pro", 16))
+		snprintf(fdt_name, I2C_BUF_SIZE, "%s",
+			 "spacemit/k1-musepi-pro");
+	else if (!strncmp(product_name, "k1-x_deb1", 9))
+		snprintf(fdt_name, I2C_BUF_SIZE, "%s",
+			 "spacemit/k1-bananapi-f3");
+	else if (!strncmp(product_name, "k1-x_milkv-jupiter", 18))
+		snprintf(fdt_name, I2C_BUF_SIZE, "%s",
+			 "spacemit/k1-milkv-jupiter");
+	if (fdt_name[0] == '\0') {
+		/* set default board name */
+		sprintf(fdt_name, "spacemit/k1-bananapi-f3");
+	}
+	for (i = 0; i < I2C_BUF_SIZE; i++) {
+		if (fdt_name[i] == '\0')
+			break;
+		fdt_name[i] = tolower(fdt_name[i]);
+	}
+	memset(product_name, 0, I2C_BUF_SIZE);
+	memcpy(product_name, fdt_name, I2C_BUF_SIZE);
 }
 
 static void clk_early_init(void)
@@ -322,14 +353,8 @@ void nor_early_init(void)
 	udelay(10);
 }
 
-void *board_spl_fit_buffer_addr(ulong fit_size, int sectors, int bl_len)
-{
-	return (void *)CONFIG_SPL_LOAD_FIT_ADDRESS;
-}
-
 void board_init_f(ulong dummy)
 {
-	u8 i2c_buf[I2C_BUF_SIZE] = { 0 };
 	int ret;
 
 	ret = spl_early_init();
@@ -344,11 +369,12 @@ void board_init_f(ulong dummy)
 	preloader_console_init();
 
 	i2c_early_init();
-	ret = read_product_name(i2c_buf, I2C_BUF_SIZE);
+	ret = read_product_name(product_name, I2C_BUF_SIZE);
 	if (ret)
 		log_info("Fail to detect board:%d\n", ret);
 	else
-		log_info("Get board name:%s\n", (char *)i2c_buf);
+		log_info("Get board name:%s\n", product_name);
+	fixup_product_name(product_name, I2C_BUF_SIZE);
 	pmic_init();
 
 	ddr_early_init();
@@ -362,4 +388,16 @@ u32 spl_boot_device(void)
 
 void spl_board_init(void)
 {
+}
+
+int board_fit_config_name_match(const char *name)
+{
+	if (!strcmp(name, product_name))
+		return 0;
+	return -ENOENT;
+}
+
+void *board_spl_fit_buffer_addr(ulong fit_size, int sectors, int bl_len)
+{
+	return (void *)CONFIG_SPL_LOAD_FIT_ADDRESS;
 }

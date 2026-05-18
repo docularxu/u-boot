8ae5f7be010f68aca0a51e8b6e55680aeed3dbef
Guodong Xu <guodong@riscstar.com>
2026-05-18 17:37:18 +0800

    board: k1: define SPL boot device order
    Override board_boot_order() to set the boot sequence as:
    SPI first, then MMC1, then MMC2. This ensures the system attempts
    to boot from SPI flash before falling back to SD card or eMMC.
    
    Signed-off-by: Guodong Xu <guodong@riscstar.com>

--- board/spacemit/k1/spl.c
+++ board/spacemit/k1/spl.c
@@ -422,6 +422,13 @@ u32 spl_boot_device(void)
 	return ret;
 }
 
+void board_boot_order(u32 *spl_boot_list)
+{
+	spl_boot_list[0] = BOOT_DEVICE_SPI;
+	spl_boot_list[1] = BOOT_DEVICE_MMC1;
+	spl_boot_list[2] = BOOT_DEVICE_MMC2;
+}
+
 void spl_board_init(void)
 {
 }
